0c1a739c5dfb8344b8a5318bd2c14bb3fb881c19
suresh <sureshbabu.gandla@gmail.com>
2026-02-08 19:54:00 +0000

    Initial Commit for project setup

---
+++ src/main/java/com/iitj/ai/FirstAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.iitj.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAssignmentApplication.class, args);
+	}
+
+}
